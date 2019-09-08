2c6b2232eb7e730cdbb9d473e24ad253ac66ec9e
jeonghun.kim <jeonghun.kim@kurlycorp.com>
2019-09-08 16:44:01 +0900

    이미 interceptor에서 로그인 보호가 되고 있으므로..

--- src/main/java/todoapp/web/user/UserRestController.java
+++ src/main/java/todoapp/web/user/UserRestController.java
@@ -1,27 +1,19 @@
 package todoapp.web.user;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import todoapp.security.UserSession;
 import todoapp.web.model.UserProfile;
 
 import javax.annotation.security.RolesAllowed;
-import java.util.Objects;
 
 @RestController
 public class UserRestController {
 
     @RolesAllowed({"ROLE_USER"})
     @GetMapping("api/user/profile")
-    public ResponseEntity<UserProfile> userProfile(UserSession userSession) {
-
-        if(Objects.nonNull(userSession)) {
-            return ResponseEntity.ok(new UserProfile(userSession.getUser()));
-        }
-
-        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();  //로그인된 사용자의 프로필
+    public UserProfile userProfile(UserSession userSession) {
+        return new UserProfile(userSession.getUser());
     }
 
 }
